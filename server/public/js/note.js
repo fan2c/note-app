c63168ba2ca9236afa6034b5a9dd3ca037753bdf
fan2c <pxf1017@hotmail.com>
2017-05-30 16:11:20 +0900

    feat: add testing view

---
+++ server/public/js/note.js
@@ -0,0 +1,95 @@
+
+function showError(resp) {
+   resp.json().then(function (result) {
+       console.log('Error: ' + result.message);
+   });
+}
+$(function () {
+   var vm = new Vue({
+       el: '#vm',
+       http: {
+           timeout: 5000
+       },
+       data: {
+           title: 'TODO List',
+           todos: [],
+           loading: false
+       },
+       created: function () {
+           this.init();
+       },
+       methods: {
+           init: function () {
+               var that = this;
+               that.loading = true;
+               that.$resource('/api/todos').get().then(function (resp) {
+                   that.loading = false;
+                   resp.json().then(function (result) {
+                       that.todos = result.todos;
+                   });
+               }, function (resp) {
+                   that.loading = false;
+                   showError(resp);
+               });
+           },
+           create: function (todo) {
+               var that = this;
+               that.$resource('/api/todos').save(todo).then(function (resp) {
+                   resp.json().then(function (result) {
+                       that.todos.push(result);
+                   });
+               }, showError);
+           },
+           update: function (todo, prop, e) {
+               var that = this;
+               var t = {
+                   name: todo.name,
+                   description: todo.description
+               };
+               t[prop] = e.target.innerText;
+               if (t[prop] === todo[prop]) {
+                   return;
+               }
+               that.$resource('/api/todos/' + todo.id).update(t).then(function (resp) {
+                   resp.json().then(function (r) {
+                       todo.name = r.name;
+                       todo.description = r.description;
+                   });
+               }, function (resp) {
+                   e.target.innerText = todo[prop];
+                   showError(resp);
+               });
+           },
+           remove: function (todo) {
+               var that = this;
+               that.$resource('/api/todos/' + todo.id).delete().then(function (resp) {
+                   var i, index = -1;
+                   for (i=0; i<that.todos.length; i++) {
+                       if (that.todos[i].id === todo.id) {
+                           index = i;
+                           break;
+                       }
+                   }
+                   if (index >= 0) {
+                       that.todos.splice(index, 1);
+                   }
+               }, showError);
+           }
+       }
+   });
+   window.vm = vm;
+   var vmAdd = new Vue({
+       el: '#vmAdd',
+       data: {
+           name: '',
+           description: ''
+       },
+       methods: {
+           submit: function () {
+               vm.create(this.$data);
+               this.name = '';
+               this.description = '';
+           }
+       }
+   });
+});
